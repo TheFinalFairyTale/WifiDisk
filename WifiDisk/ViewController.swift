fc2a497ebd05d6b42353873cf6be8f32f764c0fa
齐风修 <qifx@qifengxius-Mac-mini.local>
2017-04-01 22:49:15 +0800

    project init

---
+++ WifiDisk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WifiDisk
+//
+//  Created by 齐风修 on 2017/4/1.
+//  Copyright © 2017年 齐风修. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
